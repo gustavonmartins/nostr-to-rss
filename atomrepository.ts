646dd5ddcba8933407001def2625d55108f78c22
Gustavo Nunes Martins <martins.yo@gmail.com>
2024-11-17 18:42:05 +0100

    Userlist tdd: move dummy field items to atomrepo

--- atomrepository.ts
+++ atomrepository.ts
@@ -1,5 +1,6 @@
-import  { NDKEvent } from "@nostr-dev-kit/ndk";
+import { NDKEvent } from "@nostr-dev-kit/ndk";
 import { NostrRepository } from "./nostrrepo.ts";
+import { Feed } from "feed";
 
 class AtomRepository {
   private nostrRepo: NostrRepository;
@@ -7,6 +8,45 @@ class AtomRepository {
 
   constructor(nostrRepo: NostrRepository) {
     this.nostrRepo = nostrRepo;
+  }
+
+  getFeedPromise(): Promise<Feed> {
+    const atomFeed = new Feed({
+      title: "DUMMY FEED TITLE",
+      description: "DUMMY FEED DESCRIPTION",
+      id: "github.com/gustavonmartins/nostr-to-rss",
+      link: "github.com/gustavonmartins/nostr-to-rss",
+      updated: new Date(),
+      ttl: 60,
+      copyright:
+        "https://njump.me/npub1auwq2edy2tahk58uepwyvjjmdvkxdvmrv492xts8m2s030gla0msruxp7s",
+    });
+
+    atomFeed.addItem({
+      title: "DUMMY ITEM TITLE 1",
+      date: new Date(),
+      published: new Date(),
+      id: "1234",
+      link: `https://njump.me/}`,
+      content: "Content of the feeds item",
+      author: [{ name: "gustavonmartins" }],
+    });
+
+    atomFeed.addItem({
+      title: "DUMMY ITEM TITLE 2",
+      date: new Date(),
+      published: new Date(),
+      id: "1234",
+      link: `https://njump.me/}`,
+      content: "Content of the feeds item",
+      author: [{ name: "gustavonmartins" }],
+    });
+
+    const p: Promise<Feed> = new Promise((resolve, reject) => {
+      resolve(atomFeed);
+    });
+
+    return p;
   }
 }
 
--- feedFromUserList/controller.ts
+++ feedFromUserList/controller.ts
@@ -6,48 +6,18 @@ export async function getFeedFromDefaultList(
   atomRepo: AtomRepository,
   c: Context,
 ): Promise<Response> {
-  ///const atomFeed = await atomRepo.getFeed({ "replies": false });
-  /// return c.body(atomFeed, {
-  ///  headers: { "Content-Type": "application/atom+xml" },
-  /// });
   console.log("Running well here");
-  const atomFeed = new Feed({
-    title: "DUMMY FEED TITLE",
-    description: "DUMMY FEED DESCRIPTION",
-    id: "github.com/gustavonmartins/nostr-to-rss",
-    link: "github.com/gustavonmartins/nostr-to-rss",
-    updated: new Date(),
-    ttl: 60,
-    copyright:
-      "https://njump.me/npub1auwq2edy2tahk58uepwyvjjmdvkxdvmrv492xts8m2s030gla0msruxp7s",
-  });
 
-  atomFeed.addItem({
-    title: "DUMMY ITEM TITLE 1",
-    date: new Date(),
-    published: new Date(),
-    id: "1234",
-    link: `https://njump.me/}`,
-    content: "Content of the feeds item",
-    author: [{ name: "gustavonmartins" }],
-  });
-
-  atomFeed.addItem({
-    title: "DUMMY ITEM TITLE 2",
-    date: new Date(),
-    published: new Date(),
-    id: "1234",
-    link: `https://njump.me/}`,
-    content: "Content of the feeds item",
-    author: [{ name: "gustavonmartins" }],
-  });
-
+  //Will return the http call, but only when atom repo gave all items of feed back
   const p: Promise<Response> = new Promise((resolve, reject) => {
-    resolve(
-      c.body(atomFeed.atom1(), {
-        headers: { "Content-Type": "application/atom+xml" },
-      }),
-    );
+    const promiseFeed: Promise<Feed> = atomRepo.getFeedPromise();
+    promiseFeed.then((result) => {
+      resolve(
+        c.body(result.atom1(), {
+          headers: { "Content-Type": "application/atom+xml" },
+        }),
+      );
+    });
   });
 
   return p;
