82dd9dcadfa9c3d96daaae056446745cfb33b7fd
Gustavo Nunes Martins <martins.yo@gmail.com>
2024-11-20 00:00:01 +0100

    Preparation for link parsing

--- utils.ts
+++ utils.ts
@@ -7,3 +7,52 @@ export function getTagsMultiple(tags: string[][], key: string): string[][] {
   }
   return output;
 }
+
+export function mediafilter(text: string): { uri: string; mimeType: string }[] {
+  const mediaLinks = [];
+
+  const urlRegex = /https?:\/\/[^\s]+/g;
+  const imageExtensions = [
+    "jpeg",
+    "jpg",
+    "png",
+    "gif",
+    "bmp",
+    "webp",
+    "tiff",
+    "svg",
+  ];
+  const videoExtensions = [
+    "mov",
+    "mp4",
+    "avi",
+    "mkv",
+    "webm",
+    "flv",
+    "wmv",
+    "m4v",
+  ];
+
+  const uris = text.match(urlRegex);
+
+  if (uris) {
+    for (const uri of uris) {
+      const extension = uri.split(".").pop()?.toLowerCase();
+      if (extension) {
+        if (imageExtensions.includes(extension)) {
+          mediaLinks.push({
+            uri: uri,
+            mimeType: `image/${extension}`,
+          });
+        } else if (videoExtensions.includes(extension)) {
+          mediaLinks.push({
+            uri: uri,
+            mimeType: `video/${extension}`,
+          });
+        }
+      }
+    }
+  }
+
+  return mediaLinks;
+}
